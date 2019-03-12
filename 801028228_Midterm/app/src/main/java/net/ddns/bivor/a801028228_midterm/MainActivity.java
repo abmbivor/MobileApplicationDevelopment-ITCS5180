e55e1ecc3b08c3e869fc471b2b80fc199c72ba3a
Mohaimenur Rahman Bivor <abm.bivor@gmail.com>
2019-03-11 20:52:05 -0400

    submittable

--- 801028228_Midterm/app/src/main/java/net/ddns/bivor/a801028228_midterm/MainActivity.java
+++ 801028228_Midterm/app/src/main/java/net/ddns/bivor/a801028228_midterm/MainActivity.java
@@ -6,6 +6,16 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.commons.io.IOUtils;
@@ -24,57 +34,159 @@ import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
+    SeekBar sb;
+    Button buttonSearch;
+    RadioGroup rg;
+    RecyclerView recyclerView;
+    RecyclerView.Adapter  mAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    TextView textViewLimit;
+    TextView test;
+    ProgressBar pb;
+    EditText editText;
+    String filter="&s_track_rating=desc";
+    String keyword="";
+    int progressValue=5;
+
+    ArrayList<Track> tracks;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("MusixMatch Track Search");
+
+        tracks = new ArrayList<>();
+
+        sb = findViewById(R.id.seekBar);
+
+        pb = findViewById(R.id.progressBar);
+        pb.setVisibility(View.INVISIBLE);
+
+        rg = findViewById(R.id.radioGroup);
+
+        buttonSearch = findViewById(R.id.buttonSearch);
+
+        textViewLimit = findViewById(R.id.textViewLimit);
+
+        editText = findViewById(R.id.editText);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new TrackAdapter(tracks);
+        recyclerView.setAdapter(mAdapter);
+
+
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                textViewLimit.setText("Limit: "+ (progress+5));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                pb.setVisibility(View.VISIBLE);
+                buttonSearch.setEnabled(false);
+                rg.setEnabled(false);
+                sb.setEnabled(false);
+
+                if(checkedId==R.id.radioButtonTrack)filter = "&s_track_rating=desc";
+                else if (checkedId == R.id.radioButtonArtist)filter ="&s_artist_rating=desc";
+
+                new GetTracksAsync().execute();
+
+
+            }
+        });
+
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(isConnected()){
+                    pb.setVisibility(View.VISIBLE);
+                    buttonSearch.setEnabled(false);
+                    rg.setEnabled(false);
+                    sb.setEnabled(false);
+
+                    if(editText.getText().toString().isEmpty())editText.setError("Enter Keyword/s");
+                    else {
+                        keyword = editText.getText().toString();
+                        progressValue = (sb.getProgress()+5);
+                        mAdapter.notifyDataSetChanged();
+
+                        new GetTracksAsync().execute();
+                    }
+
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "There is no internet connection", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 
 
-    private class GetSongsAsync extends AsyncTask<Void, Void, ArrayList<Song>> {
+    private class GetTracksAsync extends AsyncTask<Void, Void, ArrayList<Track>> {
 
         @Override
-        protected ArrayList<Song> doInBackground(Void... voids) {
+        protected ArrayList<Track> doInBackground(Void... voids) {
             HttpURLConnection connection = null;
-            ArrayList<Song> result = new ArrayList<>();
+            ArrayList<Track> result = new ArrayList<>();
 
             try {
-                URL url = new URL("http://api.musixmatch.com/ws/1.1/track.search?q=" + editText.getText().toString().trim().replaceAll("\\s", "+") + "&apikey=1908e74cafedd7131cdaf8cca81c5632");
+                URL url = new URL("http://api.musixmatch.com/ws/1.1/track.search?q=" + editText.getText().toString().trim().replaceAll("\\s", "+")
+                + "&page_size=" + progressValue
+                        + filter
+                        + "&apikey=1908e74cafedd7131cdaf8cca81c5632");
                 connection = (HttpURLConnection) url.openConnection();
                 connection.connect();
+//                int test = connection.getResponseCode();
+//                String t = test+"";
                 if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
                     String json = IOUtils.toString(connection.getInputStream(), "UTF8");
-
                     JSONObject root = new JSONObject(json);
                     JSONObject message = root.getJSONObject("message");
                     JSONObject body = message.getJSONObject("body");
 
-                    JSONArray songs = body.getJSONArray("track_list");
+                    JSONArray tracks = body.getJSONArray("track_list");
 
-                    int a = songs.length();
-                    for (int i = 0; i < songs.length(); i++) {
-                        JSONObject songJson = songs.getJSONObject(i);
-                        JSONObject track = songJson.getJSONObject("track");
-                        Song song = new Song();
-                        song.trackName = (songJson.has("trackName")) ? songJson.getString("trackName") : "N/A";
-                        song.artistName = (songJson.has("artistName")) ? songJson.getString("artistName") : "N/A";
-                        song.collectionName = (songJson.has("collectionName")) ? songJson.getString("collectionName") : "N/A";
-                        song.primaryGenreName = (songJson.has("primaryGenreName")) ? songJson.getString("primaryGenreName") : "N/A";
-                        //song.trackPrice = songJson.getString("trackPrice");
-                        song.trackPrice = (songJson.has("trackPrice")) ? songJson.getString("trackPrice") : "0";
-//                        song.collectionPrice = songJson.getString("collectionPrice");
-                        song.collectionPrice = (songJson.has("collectionPrice")) ? songJson.getString("collectionPrice") : "0";
-                        song.dateTime = (songJson.has("releaseDate")) ? songJson.getString("releaseDate") : "N/A";
-                        song.artworkUrl100 = (songJson.has("artworkUrl100")) ? songJson.getString("artworkUrl100") : "N/A";
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-                        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-                        song.date = (songJson.has("releaseDate")) ? sdf.parse(songJson.getString("releaseDate")) : null;
-                        song.state = filter;
+                    for (int i = 0; i < tracks.length(); i++) {
+                        JSONObject trackJson = tracks.getJSONObject(i);
+                        JSONObject trackHeader = trackJson.getJSONObject("track");
+                        Track track = new Track();
+                        track.track_name =(trackHeader.has("track_name")) ? trackHeader.getString("track_name") : "N/A";
+                        track.artist_name =(trackHeader.has("artist_name")) ? trackHeader.getString("artist_name") : "N/A";
+                        track.album_name =(trackHeader.has("album_name")) ? trackHeader.getString("album_name") : "N/A";
+                        track.updated_time =(trackHeader.has("updated_time")) ? trackHeader.getString("updated_time") : "N/A";
+                        track.track_share_url =(trackHeader.has("track_share_url")) ? trackHeader.getString("track_share_url") : "N/A";
 
-                        result.add(song);
+                        result.add(track);
                     }
                 }
-            } catch (IOException | ParseException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -88,7 +200,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(ArrayList<Song> result) {
+        protected void onPostExecute(ArrayList<Track> result) {
+
+            tracks.clear();
+            tracks.addAll(result);
+            if(tracks.size()>0){
+                mAdapter.notifyDataSetChanged();
+                pb.setVisibility(View.INVISIBLE);
+                buttonSearch.setEnabled(true);
+                rg.setEnabled(true);
+                sb.setEnabled(true);
+            }
+            else {
+                Toast.makeText(MainActivity.this, "No Tracks Found", Toast.LENGTH_SHORT).show();
+                pb.setVisibility(View.INVISIBLE);
+                buttonSearch.setEnabled(true);
+                rg.setEnabled(true);
+                sb.setEnabled(true);
+            }
 
         }
     }
--- 801028228_Midterm/app/src/main/java/net/ddns/bivor/a801028228_midterm/TrackAdapter.java
+++ 801028228_Midterm/app/src/main/java/net/ddns/bivor/a801028228_midterm/TrackAdapter.java
@@ -1,11 +1,14 @@
 package net.ddns.bivor.a801028228_midterm;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,46 +23,55 @@ public class TrackAdapter extends RecyclerView.Adapter<TrackAdapter.ViewHolder>{
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.track_item, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
 
+        Track track = mData.get(i);
+        viewHolder.textViewItemArtist.setText("Artist: "+track.artist_name);
+        viewHolder.textViewItemTrack.setText("Track: "+track.track_name);
+        viewHolder.textViewItemAlbum.setText("Album: "+track.album_name);
+        String date = track.updated_time;
+        viewHolder.textViewItemDate.setText("Date: "+date.substring(5,7)+"-"+date.substring(8,10)+"-"+date.substring(0,4));
+        viewHolder.track = track;
+        viewHolder.trackList = mData;
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mData.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
-        TextView textViewItemTrack, textViewItemPrice, textViewItemArtist, textViewItemDate;
-        Track song;
-        ArrayList<Track> songList;
-//        String keyword, state;
-//        int progressValue;
+        TextView textViewItemTrack, textViewItemArtist, textViewItemAlbum, textViewItemDate;
+        Track track;
+        ArrayList<Track> trackList;
 
         public ViewHolder(@NonNull final View itemView) {
             super(itemView);
 
-//            textViewItemTrack = itemView.findViewById(R.id.textViewItemTrack);
-//            textViewItemPrice = itemView.findViewById(R.id.textViewItemPrice);
-//            textViewItemArtist = itemView.findViewById(R.id.textViewItemArtist);
-//            textViewItemDate = itemView.findViewById(R.id.textViewItemDate);
-//
-//            itemView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Intent intent = new Intent(itemView.getContext(), Display.class);
-//                    intent.putExtra(SONG_KEY,song);
-//                    intent.putExtra(SONGS_KEY,songList);
-////                    intent.putExtra(KEYWORD,keyword);
-////                    intent.putExtra(STATE,state);
-////                    intent.putExtra(PROGRESS_VALUE,progressValue);
-//                    itemView.getContext().startActivity(intent);
-//                }
-//            });
+            textViewItemTrack = itemView.findViewById(R.id.textViewItemTrack);
+            textViewItemArtist = itemView.findViewById(R.id.textViewItemArtist);
+            textViewItemAlbum = itemView.findViewById(R.id.textViewItemAlbum);
+            textViewItemDate = itemView.findViewById(R.id.textViewItemDate);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    if(track.track_share_url!="N/A") {
+                        intent.setData(Uri.parse(track.track_share_url));
+                        itemView.getContext().startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(itemView.getContext(), "No Track Share URL Found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
 
         }
     }
